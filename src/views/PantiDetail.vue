2a48bfa190accda7f9f4527e04afcb3e9fded33c
Christopher Nathanael Tessy <christopher.nathanael1217@gmail.com>
2025-03-07 13:16:31 +0700

    feat: add panti region to panti detail page

--- src/views/PantiDetail.vue
+++ src/views/PantiDetail.vue
@@ -105,6 +105,34 @@
           {{ panti.name || "" }}
         </h1>
 
+        <span
+          class="flex justify-center items-center gap-0.5 text-secondary-500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6 lg:size-7 xl:size-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+            />
+          </svg>
+
+          <p class="text-lg lg:text-xl xl:text-2xl">
+            {{ panti.region }}
+          </p>
+        </span>
+
         <p v-if="panti" class="text-lg lg:text-xl xl:text-2xl">
           {{ panti.address || "" }}
         </p>
