45d766c2aa871766429fe80fb8334de98da25dcb
jasonlairenz <jason.lairenz@gdn-commerce.com>
2025-03-14 12:19:34 +0700

    feat: add donatur page donasi saya

---
+++ src/components/cards/FundDonationCard.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <LoadingIndicator v-if="fetching" 
+      text="Memuat data donasi..." 
+      color="text-secondary-500" 
+    />
+    
+    <p v-else-if="!fetching && fundDonationList.length === 0" 
+    class="text-center text-red-500 mt-4">
+      Tidak ada data donasi tersedia.
+    </p>
+
+    <!-- Card Container -->
+    <div v-else class="flex flex-col items-center space-y-4">
+      <div v-for="donation in paginatedData" :key="donation.id"
+        class="bg-white shadow-md rounded-xl p-4 border border-gray-200 w-full max-w-[600px] lg:max-w-full flex flex-col h-full"
+      >
+        <div>
+          <h2 class="text-lg font-semibold text-gray-800">
+            {{ donation.panti_name || '-' }}
+          </h2>
+
+          <p class="text-sm text-gray-600">
+            <span class="font-medium">Tanggal Donasi:</span> {{ donation.donation_date }}
+          </p>
+
+          <p class="text-sm text-gray-600 mb-2">
+            <span class="font-medium">Nominal:</span> Rp.{{ donation.nominal_amount.toLocaleString() }}
+          </p>
+        </div>
+
+        <!-- Status dan Tombol di Satu Baris -->
+        <div class="mt-3 flex justify-between items-center">
+          <p class="text-sm">
+            <span class="font-medium">Status: </span>
+            <span 
+              :class="{
+                'bg-yellow-200 text-yellow-700': donation.status === 'PENDING',
+                'bg-green-200 text-green-700': donation.status === 'COMPLETED',
+                'bg-red-200 text-red-700': donation.status === 'REJECTED'
+              }"
+              class="px-2 py-1 rounded text-xs font-semibold"
+            >
+              {{ donation.status }}
+            </span>
+          </p>
+
+          <router-link 
+            :to="{ name: 'pantiDetail', params: { id: donation.panti_id } }"
+            class="text-blue-600 hover:underline px-4 py-2 rounded-md text-sm font-medium"
+          >
+            Lihat Panti Asuhan →
+          </router-link>
+        </div>
+      </div>
+    </div>
+
+    <!-- Pagination -->
+    <div class="mt-auto flex justify-end">
+      <Pagination v-if="fundDonationList.length > 0" 
+        :totalItems="fundDonationList.length" 
+        :itemsPerPage="itemsPerPage" 
+        v-model:currentPage="currentPage" 
+        class="mt-4" 
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import { fetchFundDonationsById } from "@/services/api-donation";
+import LoadingIndicator from "@/components/loading/LoadingIndicator.vue";
+import Pagination from "@/components/pagination/PaginationDashboard.vue";
+import { useAuthStore } from "@/stores/authStore";
+
+const authStore = useAuthStore();
+
+const fundDonationList = ref([]);
+const fetching = ref(true);
+const currentPage = ref(1);
+const itemsPerPage = 10;
+
+const paginatedData = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage;
+  return fundDonationList.value.slice(start, start + itemsPerPage);
+});
+
+onMounted(async () => {
+  try {
+    const data = await fetchFundDonationsById(authStore ? authStore.user.id : 0);
+    fundDonationList.value = data.fund_donation_responses;
+  } catch (error) {
+    console.error("Error fetching fund donation data:", error);
+  } finally {
+    fetching.value = false;
+  }
+});
+</script>
--- src/views/user/DonasiSaya.vue
+++ src/views/user/DonasiSaya.vue
@@ -1,11 +1,86 @@
 <template>
-  <section
-    class="bg-primary-50 text-secondary-500 h-screen w-full flex justify-center items-center"
-  >
-    <h1 class="text-4xl font-bold">Donasi Saya</h1>
+  <section class="min-h-screen pt-36">
+    <div v-if="!isDonatur" class="p-6">
+      <h1 class="text-4xl font-bold text-center text-red-700 justify-center">
+        Kamu Tidak Memiliki Akses
+      </h1>
+    </div>
+
+    <div v-else class="p-6">
+      <!-- Profile Section Placeholder -->
+      <div class="flex flex-col md:flex-row items-center justify-center gap-6 mb-10 md:mb-20">
+        <!-- Profile Picture Placeholder -->
+        <img 
+          :src="`${apiUrl}/storage/profile/${authStore.user.profile}`"
+          :alt="`${authStore.user?.name || 'User'}'image`" 
+          class="w-40 h-40 md:w-52 md:h-52 rounded-full border-2 border-gray-300"
+        />
+        
+        <!-- User Info Placeholder -->
+        <div class="text-center md:text-left">
+          <h2 class="text-2xl font-bold">{{ authStore.user.name }}</h2>
+          <p class="text-gray-600">Email: {{ authStore.user.email }}</p>
+          <p class="text-gray-600">Telepon: {{ authStore.user.phone }}</p>
+          <p class="text-gray-600">Alamat: {{ authStore.user.address }}</p>
+          <div class="mt-4 md:text-right">
+            <button class="px-4 py-2 bg-blue-700 text-white rounded-xl hover:bg-blue-400 transition duration-300">
+              Edit Profile
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Toggle Buttons -->
+      <div class="flex flex-wrap justify-start gap-4 mb-4">
+        <button @click="activeTabDonatur = 'fund-donation'" 
+          :class="{
+            'bg-blue-700 text-white rounded-lg': activeTabDonatur === 'fund-donation',
+            'bg-gray-300 rounded-lg hover:bg-blue-600': activeTabDonatur !== 'fund-donation'
+            }"
+          class="px-4 py-2  transition duration-300">
+          Donasi Dana
+        </button>
+
+        <button @click="activeTabDonatur = 'nonfund-donation'" 
+          :class="{
+            'bg-blue-700 text-white rounded-lg': activeTabDonatur === 'nonfund-donation',
+            'bg-gray-300 rounded-lg hover:bg-blue-600': activeTabDonatur !== 'nonfund-donation'
+            }"
+          class="px-4 py-2  transition duration-300">
+          Donasi Non-Dana
+        </button>
+        
+      </div>
+
+      <!-- Conditional Rendering -->
+      <FundDonationCard v-if="activeTabDonatur === 'fund-donation'" class="relative" />
+      <NonFundDonationCard v-else-if="activeTabDonatur === 'nonfund-donation'" class="relative" />
+    </div>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, computed, onMounted, watch } from "vue";
+import FundDonationCard from "@/components/cards/FundDonationCard.vue";
+import NonFundDonationCard from "@/components/cards/NonFundDonationCard.vue";
+import { useAuthStore } from "@/stores/authStore";
 
-<style></style>
+const authStore = useAuthStore();
+const activeTabDonatur = ref("fund-donation");
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const isDonatur = computed(() => {
+  return authStore.user?.role === "ROLE_DONATUR" ? true : false;
+});
+
+onMounted(() => {
+  const savedTab = localStorage.getItem("activeTabDonatur");
+  if (savedTab) {
+    activeTabDonatur.value = savedTab;
+  }
+});
+
+watch(activeTabDonatur, (newValue) => {
+  localStorage.setItem("activeTabDonatur", newValue);
+});
+</script>
