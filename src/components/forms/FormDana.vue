c3e9c749d3f15a6ca288c125a6522a54ed51f038
joshuajevon <joshuajevon05@gmail.com>
2025-03-16 11:57:13 +0700

    fix: input file form

--- src/components/forms/FormDana.vue
+++ src/components/forms/FormDana.vue
@@ -148,16 +148,41 @@
             >
               Bukti Transfer
             </label>
-
-            <input
-              type="file"
-              id="proof_of_payment"
-              name="proof_of_payment"
-              placeholder="Masukkan bukti transfer Anda"
-              accept="image/*"
-              @change="handleFileUpload"
-              ref="fileInput"
-            />
+            
+            <div class="flex items-center justify-center w-full">
+              <label
+                for="proof_of_payment"
+                class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-3xl cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    class="w-8 h-8 mb-4 text-secondary-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                  </svg>
+                  <p v-if="fileName" class="mb-2 mx-2 text-center text-sm font-semibold text-green-500">
+                    File terpilih: {{ fileName }}
+                  </p>
+                  <p v-else class="mb-2 text-sm text-secondary-500">
+                    <span class="font-semibold">Klik untuk mengunggah</span>
+                  </p>
+                  <p class="text-xs text-red-500">Maksimum 2 MB</p>
+                  <p class="text-xs text-red-500">JPEG, PNG, JPG</p>
+                </div>
+                <input
+                  type="file"
+                  id="proof_of_payment"
+                  name="proof_of_payment"
+                  accept="image/*"
+                  class="hidden"
+                  @change="handleFileUpload"
+                />
+              </label>
+            </div> 
 
             <p
               v-if="errorMessages.image"
@@ -238,10 +263,18 @@ const errorMessages = reactive({
 const isSubmitting = ref(false);
 
 const fileInput = ref(null);
+const fileName = ref("");
 
 const handleFileUpload = (event) => {
   const file = event.target.files[0];
+
+  if (!file) {
+    fileName.value = ""; 
+    return;
+  }
+
   if (file) {
+    fileName.value = file.name;
     donationData.image = file;
   }
 };
