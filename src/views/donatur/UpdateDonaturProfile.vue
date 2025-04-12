f105ac4a037add39648c25ed325c916e8d23db24
joshuajevon <joshuajevon05@gmail.com>
2025-04-12 10:23:43 +0700

    fix: profile path

--- src/views/donatur/UpdateDonaturProfile.vue
+++ src/views/donatur/UpdateDonaturProfile.vue
@@ -31,7 +31,7 @@
       <div class="flex flex-col items-center">
         <div class="relative">
           <img
-            :src="previewImage || `${apiUrl}/storage/profile/${form.profile}`"
+            :src="previewImage || `${apiUrl}/storage/profile/${authStore.user.profile}`"
             alt="Profile Picture"
             class="w-40 h-40 rounded-full object-cover border-2 border-gray-300 bg-white p-2 shadow"
           />
