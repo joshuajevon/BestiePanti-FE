3ea86f943f4019f3a8406110927048d0a7ccc5e3
jasonlairenz <jason.lairenz@gdn-commerce.com>
2025-04-24 00:16:36 +0700

    fix: update button add panti to plus button

--- src/components/dashboard-admin/PantiTable.vue
+++ src/components/dashboard-admin/PantiTable.vue
@@ -168,9 +168,11 @@
 
           <button
             @click="$router.push({ name: 'tambah-panti' })"
-            class="px-4 py-2 bg-blue-700 text-white text-sm font-medium rounded-xl shadow-md hover:bg-blue-600 transition duration-200"
+            class="w-10 h-10 flex items-center justify-center bg-blue-700 text-white rounded-full shadow-md hover:bg-blue-500 transition duration-200"
           >
-            Tambah Panti
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+            </svg>
           </button>
         </div>
       </div>
